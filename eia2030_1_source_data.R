e4592cb9a720acd662d0bfbe3f09a5c766f39d4a
egbendito <e.gbendito@gmail.com>
2021-03-16 08:35:00 +0000

    Updated NASA POWER Function

--- eia2030_1_source_data.R
+++ eia2030_1_source_data.R
@@ -1,13 +1,13 @@
-library(tidyverse)
-library(nasapower)
-library(sf)
-library(raster)
-library(stars)
-
-nasaP <- function(xmin, ymin, xmax, ymax, sdate, edate, par1, par2, par3, par4){
+## Access NASA POWER API
+nasaP <- function(xmin, ymin, xmax, ymax, sdate, edate, par1, par2, par3, par4, tr){ # For the moment 4 parameters. This can be extended later, or make it more flexible to allow the user to select parameters.
+  require(nasapower)
+  require(sf)
+  require(raster)
+# Fetch NASA POWER data  
   tbl <- get_power(
     community = "AG",
-    lonlat = c(xmin, ymin, xmax, ymax),
+    lonlat = c(xmin, ymin, xmax, ymax), # BBOX for the ROI. There is a limit on the size of the BBOX.
+# This collects the info from the indicated parameters
     pars = if(missing(par4) & missing(par3) & missing(par2)) {
       c(par1)
     } else if (missing(par4) & missing(par3)) {
@@ -20,7 +20,10 @@ nasaP <- function(xmin, ymin, xmax, ymax, sdate, edate, par1, par2, par3, par4){
     dates = c(sdate, edate),
     temporal_average = "DAILY"
     )
-  tbl <- if(missing(par4) & missing(par3) & missing(par2)) {
+# Create object as a raw table from the POWER API
+  assign("table", tbl, envir = .GlobalEnv)
+# Format table removing unnecessary columns (i.e.: DOY, YEAR, MONTH, etc)
+    tbl <- if(missing(par4) & missing(par3) & missing(par2)) {
     dplyr::select(tbl,LON,LAT,YYYYMMDD,par1)
   } else if (missing(par4) & missing(par3)) {
     dplyr::select(tbl,LON,LAT,YYYYMMDD,par1,par2)
@@ -29,30 +32,48 @@ nasaP <- function(xmin, ymin, xmax, ymax, sdate, edate, par1, par2, par3, par4){
   } else {
     dplyr::select(tbl,LON,LAT,YYYYMMDD,par1,par2,par3,par4)
   }
+# Create a sf object with POINT_GEOMETRY
   pnt <- st_as_sf(tbl, coords = c("LON", "LAT"), crs = 4326)
-  # strs <- st_as_stars(pnt)
-  # 
-  # st <- stack()
-  # n <- 1
-  # for(i in names(pnt)[!grepl("['geometry]", names(pnt))]){
-  #   vals <- pull(pnt, i) # Read vector with`` the values of the date (i)
-  #   print(vals)
-  #   # r <- raster(ext = extent(st_bbox(pnt)) + 0.5, crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0",
-  #   #             resolution = tr) # Create Raster object with the desired Target Resolution
-  #   # ras <- rasterize(x = pnt,
-  #   #                  y = raster(ext = extent(st_bbox(pnt)) + 0.5, resolution = 0.5),
-  #   #                  field = vals)
-  #   # crs(ras) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-  #   # ras <- resample(ras, r, method = "bilinear")
-  #   # st <- stack(st,ras)
-  #   # names(st[[n]]) <- as.character(i) # https://stackoverflow.com/questions/36844460/why-does-r-add-an-x-when-renaming-raster-stack-layers
-  #   # remove(vals,ras)
-  #   # n <- n + 1
-  # }
-  # # br <- terra::rast(brick(st))
-  
-  return(pnt)
+  assign("points", pnt, envir = .GlobalEnv)
+# Create new objects for each parameter selected  
+  lapply(names(pnt)[!grepl(paste(c("YYYYMMDD","geometry"), collapse="|"), names(pnt))], function(x) assign(x, pnt[c("YYYYMMDD",x)], envir = .GlobalEnv))
+  n <- 0
+# Rasterize the sf of each parameter at a selected resolution (tr)
+  for(i in names(pnt)[!grepl(paste(c("YYYYMMDD", "geometry"), collapse="|"), names(pnt))]){
+    r <- raster(ext = extent(st_bbox(pnt)) + 0.5, crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0", resolution = 0.5)
+    st <- stack()
+    j <- get(i, envir = .GlobalEnv)
+    j <-tidyr::spread(j, "YYYYMMDD", i)
+    ## Re-format column labels
+    colnames(j) <- gsub('-', '_', colnames(j), fixed=TRUE)
+    for(k in names(j)[!grepl("['geometry]", names(j))]){
+      vals <- dplyr::pull(j, k) # Read vector with`` the values of the date (k)
+      r <- raster(ext = extent(st_bbox(j)) + 0.5, crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0",
+                  resolution = tr) # Create Raster object with the desired Target Resolution
+      ras <- rasterize(x = j,
+                       y = raster(ext = extent(st_bbox(j)) + 0.5, resolution = 0.5),
+                       field = vals)
+      crs(ras) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+      ras <- resample(ras, r, method = "bilinear")
+      st <- stack(st,ras)
+      # names(st[[n]]) <- as.character(k) # https://stackoverflow.com/questions/36844460/why-does-r-add-an-x-when-renaming-raster-stack-layers
+# Create RasterStack of each object       
+      assign(paste0(i), st , envir = .GlobalEnv)
+      remove(vals,ras)
+      n <- n + 1
+    }
+  }
 }
 
-d <- nasaP(36.5, -1.5 , 37.5, -1.0, "2020-01-01", "2020-01-30", "T10M", "T2M", "RH2M")
-strs <- st_as_stars(d)
+nasaP(35.5, -1.5 , 37.5, 1.0, "2020-01-01", "2020-01-30", "T10M", "T2M", "RH2M", tr = 0.00833)
+
+# library(leaflet)
+# ## Plot on Leaflet
+# leaflet() %>%
+#   addTiles() %>% # Add default OpenStreetMap map tiles
+#   # addRasterImage(raster(full.dataset$datacube[[1]]), colors = rev(terrain.colors(10)), opacity = 0.8, project = FALSE) %>%
+#   # addRasterImage(mask(raster(d$datacube[[1]]), st_as_sf(aoi$geometry)),
+#   #                colors = rev(terrain.colors(10)), opacity = 0.8, project = FALSE) %>%
+#   # addPolygons(data = aoi$geometry, weight = 2, fillColor = 'transparent')
+#   addRasterImage(r, colors = rev(terrain.colors(10)), opacity = 0.8, project = FALSE) %>%
+#   addMarkers(data = d$geometry)
