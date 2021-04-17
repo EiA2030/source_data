c6bb39acfd3235d575e583e4c9c61edd54356823
egbendito <e.gbendito@gmail.com>
2021-04-17 16:58:28 +0000

    split rgee_source_data.R into R/daily_data_gee.R and R/extract_daily_data.R

---
+++ R/daily_data_gee.R
@@ -0,0 +1,52 @@
+library(rgee)
+
+ee_Initialize(display = TRUE)
+
+# Function for ImageCollections with daily data 
+daily.IC <- function(imcol,band,sdate,edate,xmin,ymin,xmax,ymax){
+  return(ee$ImageCollection(imcol)$
+           select(band)$
+           filterDate(sdate, edate)$
+           filterBounds(ee$Geometry$BBox(xmin,ymin,xmax,ymax)))
+}
+# Function to aggregate Image Collections with sub-daily data
+aggDaily.IC <- function(imcol,band,sdate,edate,xmin,ymin,xmax,ymax){
+  len <- length(as.character(seq(as.Date(sdate), as.Date(edate), "days")))
+  return(ee$ImageCollection(
+    ee$List$sequence(0,len)$map(
+      ee_utils_pyfunc(
+        function(n) {
+          s <- ee$Date(sdate)$advance(n, 'day')
+          e <- s$advance(1, 'day')
+          return(ee$ImageCollection(imcol)$
+                   filterDate(s, e)$
+                   select(band)$
+                   filterBounds(ee$Geometry$BBox(xmin,ymin,xmax,ymax))$
+                   mean()$
+                   set('system:time_start', s$millis())$
+                   set('system:index', s$format("YYYYMMdd"))
+          )
+        }
+      )
+    )
+  )
+  )
+}
+
+###########################               EXAMPLES                 ##########################
+
+# Daily precipitation (mm) data from CHIRPS
+prec <- daily.IC(imcol = "UCSB-CHG/CHIRPS/DAILY", band = "precipitation", sdate = "2010-01-01", edate = "2015-12-31", xmin = 34.8145177, ymin = -15.3265231, xmax = 35.3005743, ymax = -14.77034)
+# Daily temperature (C) from ECMWF
+tmin <- daily.IC(imcol = "ECMWF/ERA5/DAILY", band = "minimum_2m_air_temperature", sdate = "2010-01-01", edate = "2015-12-31", xmin = 34.8145177, ymin = -15.3265231, xmax = 35.3005743, ymax = -14.77034)
+# Daily temperature (C) from ECMWF
+tmax <- daily.IC(imcol = "ECMWF/ERA5/DAILY", band = "maximum_2m_air_temperature", sdate = "2010-01-01", edate = "2015-12-31", xmin = 34.8145177, ymin = -15.3265231, xmax = 35.3005743, ymax = -14.77034)
+# Daily wind U (m/s) from ECMWF
+wind.U <- daily.IC(imcol = "ECMWF/ERA5/DAILY", band = "u_component_of_wind_10m", sdate = "2010-01-01", edate = "2015-12-31", xmin = 34.8145177, ymin = -15.3265231, xmax = 35.3005743, ymax = -14.77034)
+# Daily wind V (m/s) from ECMWF
+wind.V <- daily.IC(imcol = "ECMWF/ERA5/DAILY", band = "v_component_of_wind_10m", sdate = "2010-01-01", edate = "2015-12-31", xmin = 34.8145177, ymin = -15.3265231, xmax = 35.3005743, ymax = -14.77034)
+# Daily average water vapor pressure from (NCEP)
+vapr <- aggDaily.IC(imcol = "NCEP_RE/surface_wv", band = "pr_wtr", sdate = "2010-01-01", edate = "2015-12-31", xmin = 34.8145177, ymin = -15.3265231, xmax = 35.3005743, ymax = -14.77034)
+# Daily average solar net radiation () from ECMWF
+srad <- aggDaily.IC(imcol = "ECMWF/ERA5_LAND/HOURLY", band = "surface_net_solar_radiation", sdate = "2010-01-01", edate = "2015-12-31", xmin = 34.8145177, ymin = -15.3265231, xmax = 35.3005743, ymax = -14.77034)
+
