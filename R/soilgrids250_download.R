e06add929733adcdae1c43c86efc8afae253ae49
egbendito <e.gbendito@gmail.com>
2021-06-03 09:37:45 +0000

    Update soilgrids250_download.R

--- R/soilgrids250_download.R
+++ R/soilgrids250_download.R
@@ -6,26 +6,18 @@
     # depth: Depth of the layer to request. Default is 0-5 cm interval
     # xmin,ymin,xmax,ymax: Bounding Box parameters to defne the spatial extent of the request 
 soilgrids250_data <- function(par, depth = '0-5', xmin, ymin, xmax, ymax){
-  require(sf)
+  require(rgdal)
+  require(gdalUtils)
   require(terra)
-  out <- rast(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, res = c(0.002259887, 0.002389486))
-  for (x in seq(xmin, xmax, by = 2)) {
-    for (y in seq(ymin, ymax, by = 2)) {
-      x2 = x + 2
-      y2 = y + 2
-      aoi <- vect(st_as_sf(st_as_sfc(st_bbox(c(xmin = x, xmax = x2, ymin = y, ymax = y2), crs = st_crs(4326)))))
-      url <- paste0("https://maps.isric.org/mapserv?map=/map/ocd.map&SERVICE=WCS&VERSION=2.0.1&REQUEST=GetCoverage&COVERAGEID=",
-                    par,"_",depth,"cm_mean&FORMAT=image/tiff&SUBSET=",
-                    "long(",x,",",x2,")&SUBSET=",
-                    "lat(",y,",",y2,")",
-                    "&SUBSETTINGCRS=http://www.opengis.net/def/crs/EPSG/0/4326&OUTPUTCRS=http://www.opengis.net/def/crs/EPSG/0/4326")
-      download.file(url, paste0(paste('tmp',par,depth,x,y, sep = '_'), '.tif'), overwrite=TRUE)
-      tif <- crop(rast(paste0(paste('tmp',par,depth,x,y, sep = '_'), '.tif')), aoi, filename = paste0(paste(par,depth,x,y, sep = '_'), '.tif'), overwrite = T)
-      out <- terra::mosaic(out,tif,fun="mean")
-    }
-  }
-  del <- list.files(getwd(), full.names = T, pattern = "tmp_*")
-  file.remove(del)
-  writeRaster(out, paste(paste(par,depth,sep = "_"), ".tif", sep = ""), overwrite=TRUE)
-  return(out)
+  bb <- c(xmin, ymax, xmax, ymin)
+  crs <- 'EPSG:4326'
+  sg_url <- "/vsicurl?max_retry=3&retry_delay=1&list_dir=no&url=https://files.isric.org/soilgrids/latest/data/"
+  tif <- gdal_translate(paste0(sg_url, par, '/', par, '_', depth, 'cm_mean.vrt'),
+                        paste0(paste('tmp',par,depth, sep = '_'), '.tif'),
+                        tr = c(250,250),
+                        projwin=bb,
+                        projwin_srs=crs,
+                        verbose=TRUE)
+  tif <- rast(paste0(paste('tmp',par,depth, sep = '_'), '.tif'))
+  return(tif)
 }
