96571f9bb98f213d80f7abf46fcb7dff9eed1106
EduardoGB <52239752+egbendito@users.noreply.github.com>
2022-08-30 12:26:02 +0300

    Update gadm_download.R

--- R/gadm_download.R
+++ R/gadm_download.R
@@ -10,7 +10,7 @@ gadm36.download <- function(iso = NULL, path = tempdir()){
   url <- paste0("https://geodata.ucdavis.edu/gadm/gadm3.6/gpkg/gadm36_", iso, "_gpkg.zip")
   download.file(url, destfile = paste0(path,"/raw/gadm36_", iso, "_gpkg.zip"))
   unzip(paste0(path,"/raw/gadm36_", iso, "_gpkg.zip"), exdir = path)
-  for (l in sf::st_layers(paste0(path,"/raw/gadm36_", iso, ".gpkg")[1]) {
+  for (l in sf::st_layers(paste0(path,"/raw/gadm36_", iso, ".gpkg")[1])) {
     for (k in l) {
       pol <- sf::st_read(fname, layer = k, quiet = T)
       if(isFALSE(sf::st_is_valid(pol))){sf::sf_write(obj = sf::st_make_valid(pol), dsn = l, layer = k, append = FALSE)}
@@ -23,7 +23,7 @@ gadm41.download <- function(iso = NULL, path = tempdir()){
   url <- paste0("https://geodata.ucdavis.edu/gadm/gadm4.1/gpkg/gadm41_", iso, "_gpkg.zip")
   download.file(url, destfile = paste0(path,"/raw/gadm41_", iso, "_gpkg.zip"))
   unzip(paste0(path,"/raw/gadm41_", iso, "_gpkg.zip"), exdir = path)
-  for (l in sf::st_layers(paste0(path,"/raw/gadm41_", iso, ".gpkg")[1]) {
+  for (l in sf::st_layers(paste0(path,"/raw/gadm41_", iso, ".gpkg")[1])) {
     for (k in l) {
       pol <- sf::st_read(fname, layer = k, quiet = T)
       if(isFALSE(sf::st_is_valid(pol))){sf::sf_write(obj = sf::st_make_valid(pol), dsn = l, layer = k, append = FALSE)}
