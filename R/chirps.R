32e1b4b7252b1ca906633312635256b5ff42e701
Eduardo GB <e.gbendito@gmail.com>
2023-05-23 15:15:37 +0000

    correct chirps

--- R/chirps.R
+++ R/chirps.R
@@ -1,27 +1,42 @@
-chirps <- function(startDate, endDate, df = NULL){
+chirps <- function(startDate, endDate, raster = FALSE, coordPoints = NULL){
   dates <- seq.Date(as.Date(startDate, format = "%Y-%m-%d"), as.Date(endDate, format = "%Y-%m-%d"), by = "day")
   year <- unique(format(dates, "%Y"))
   chirps <- terra::rast()
-  for (file in list.files('/home/jovyan/AgWise/rawData/2_weather/rain_chirps/raw', pattern = paste0(year, collapse = '|'), full.names = TRUE)) {
+  for (file in list.files("/home/jovyan/common_data/chirps/netcdf", pattern = paste0(year, collapse = '|'), full.names = TRUE)) {
     terra::add(chirps) <- terra::rast(file)
   }
   names(chirps) <- as.character(format(as.Date(terra::time(chirps)), "%Y%m%d"))
   chirps <- chirps[[as.character(format(dates, format = "%Y%m%d"))]]
-  w <- data.frame()
-  for (pnt in seq(1:nrow(df))){
-    lon <- df[pnt, 1]
-    lat <- df[pnt, 2]
-    z <- terra::extract(chirps,data.frame(lon,lat))
-    out <- data.frame("dates" = dates)
-    out$X <- lon
-    out$Y <- lat
-    out$RAIN <- as.vector(t(z[2:length(z)]))
-    out <- data.frame("X" = out$X, "Y" = out$Y, "dates" = out$dates,
-                    "year" = format(as.Date(out$dates), format = "%Y"),
-                    "month" = format(as.Date(out$dates), format = "%m"),
-                    "day" = format(as.Date(out$dates), format = "%d"),
-                    "RAIN" = out$RAIN)
-    w <- rbind(w, out)
+  if (raster){
+    aoi <- suppressWarnings(terra::vect(sf::st_as_sf(sf::st_as_sfc(sf::st_bbox(c(xmin = min(coordPoints[,1]), xmax = max(coordPoints[,1]), ymax = max(coordPoints[,2]), ymin = min(coordPoints[,2])), crs = sf::st_crs(4326))))))
+    chirps <- terra::crop(chirps,aoi)
+    return(chirps)
   }
-  return(w)
+  else {
+    w <- data.frame()
+    for (pnt in seq(1:nrow(coordPoints))){
+      lon <- coordPoints[pnt, 1]
+      lat <- coordPoints[pnt, 2]
+      z <- terra::extract(chirps,data.frame(lon,lat))
+      out <- data.frame("dates" = dates)
+      out$X <- lon
+      out$Y <- lat
+      out$RAIN <- as.vector(t(z[2:length(z)]))
+      out <- data.frame("X" = out$X, "Y" = out$Y, "dates" = out$dates,
+                      "year" = format(as.Date(out$dates), format = "%Y"),
+                      "month" = format(as.Date(out$dates), format = "%m"),
+                      "day" = format(as.Date(out$dates), format = "%d"),
+                      "rain" = out$RAIN)
+      w <- rbind(w, out)
+    }
+    return(w)
+  }
+}
+
+fill.na <- function(x, i=5) { # For larger windows i needs to be increased (https://gis.stackexchange.com/questions/181011/fill-the-gaps-using-nearest-neighbors)
+  if( is.na(x)[i] ) {
+    return( round(mean(x, na.rm=TRUE),0) )
+  } else {
+    return( round(x[i],0) )
+  }
 }
